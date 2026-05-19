280da4211f0ac0be828deb0058ed7b45b92c390f
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-19 20:27:32 +0200

    add index page

--- 2/monthly_challanges/challanges/urls.py
+++ 2/monthly_challanges/challanges/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # URL config
 urlpatterns = [
+    path('', views.index),
     path('<int:month>', views.monthly_challange_by_num),
     path('<str:month>', views.monthly_challange, name='monthly-challange'),
 ]
--- 2/monthly_challanges/challanges/views.py
+++ 2/monthly_challanges/challanges/views.py
@@ -4,29 +4,41 @@ from django.urls import reverse
 
 
 monthly_challanges = {
-    'jan': "Code for 20 minutes a day!",
-    'feb': "Exercise every day",
-    'mar': "Go for a walk every day!",
-    'apr': "Eat breakfast every day!",
+    'january': "Code for 20 minutes a day!",
+    'february': "Exercise every day",
+    'march': "Go for a walk every day!",
+    'april': "Eat breakfast every day!",
     'may': "Go to sleep before 11pm every day!",
-    'jun': "Tell youreself 'I matter' every day!",
-    'jul': "Learn something new every day!",
-    'aug': "Drink water every day!",
-    'sep': "Clean something every day!",
-    'oct': "Do breathing exercises every day!",
-    'nov': "Learn a new skill this month!",
-    'dec': "Turn off phone for 1h every day!",
+    'june': "Tell youreself 'I matter' every day!",
+    'july': "Learn something new every day!",
+    'august': "Drink water every day!",
+    'september': "Clean something every day!",
+    'octtober': "Do breathing exercises every day!",
+    'november': "Learn a new skill this month!",
+    'december': "Turn off phone for 1h every day!",
 }
 
 
 # Create your views here.
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challanges.keys())
+
+    for month in months:
+        month_path = reverse('monthly-challange', args=[month])
+        list_items += f'<li><h3><a href="{month_path}">{month.capitalize()}</a></h3></li>\n'
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challange_by_num(request, month):
     months = list(monthly_challanges.keys())
 
     if month > len(months) or month <= 0:
-        return HttpResponseNotFound(f'404 {month} is not a valid month!')
+        return HttpResponseNotFound(f'<h1>404 {month} is not a valid month!</h1>')
     redirect_month = months[month-1]
     # Create dynamic url
     redirect_url = reverse('monthly-challange', args=[redirect_month])
@@ -36,6 +48,7 @@ def monthly_challange_by_num(request, month):
 def monthly_challange(request, month):
     try:
         challange_text = monthly_challanges[month]
-        return HttpResponse(challange_text)
+        response_data = f'<h1>{challange_text}</h1>'
+        return HttpResponse(response_data)
     except KeyError:
-        return HttpResponseNotFound(f'404 {month} is not a valid month!')
+        return HttpResponseNotFound(f'<h1>404 {month} is not a valid month!</h1>')
